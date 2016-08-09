3d3c227b039a408ea895e51b39ff0b7b6e5d268d
Sue Bourque <susanebourque@gmail.com>
2016-08-09 15:30:42 -0400

    Create homepage_landing.php

---
+++ admin/home/homepage_landing.php
@@ -0,0 +1,14 @@
+<p><span>Researchers have connected the dots between some key proteins involved in </span><a href="https://twitter.com/hashtag/Parkinsons?src=hash" class="twitter-hashtag pretty-link js-nav"><span>#</span>Parkinsons</a><span>: </span><a href="https://t.co/7JNdsU2qhD" class="twitter-timeline-link" target="_blank" rel="nofollow" title="http://bit.ly/1SxBh1s"><span class="tco-ellipsis"></span><span class="invisible">http://</span><span class="js-display-url">bit.ly/1SxBh1s</span><span class="invisible"></span><span class="tco-ellipsis"><span class="invisible"> </span></span></a><span> </span></p>
+<p><a href="https://twitter.com/MichaelJFoxOrg" target="_blank">FOLLOW US ON TWITTER</a> <a href="https://twitter.com/MichaelJFoxOrg" class="img" target="_blank"><img src="/images/hp_images/rfi-more-arrow.gif" alt="" class="rfi-inline1" width="15" height="12" /></a></p>
+
+<p><span><span><span><span>What the research says about medical marijuana and Parkinson's disease</span>: <a href="https://www.michaeljfox.org/foundation/news-detail.php?ask-the-md-medical-marijuana-and-parkinson-disease&amp;os_cid=fb-a30U0000000OWuy&amp;s_src=MJFFfb&amp;s_subsrc=askmd_marijuana">http://bit.ly/1QY24m7 </a></span></span></span></p>
+<p><a href="https://www.facebook.com/michaeljfoxfoundation" target="_blank">LIKE US ON FACEBOOK</a> <a href="https://twitter.com/MichaelJFoxOrg" class="img" target="_blank"><img src="/images/hp_images/rfi-more-arrow.gif" alt="" class="rfi-inline1" width="15" height="12" /></a></p>
+
+<div id="cmsVideo">
+<p class="rfi-l168-16"><a href="https://www.youtube.com/watch?v=w9qSu23Knqc&amp;list=PLBD81828642569CA0&amp;index=16"><img src="/files/tdf-video-homepage.jpg" width="150" height="76" /></a></p>
+<div class="rfi-l150">
+<p class="rfi-dark rfi-pad5b">Hop on a bike and join the 2016 Tour de Fox!</p>
+<p><a href="https://www.youtube.com/watch?v=w9qSu23Knqc&amp;list=PLBD81828642569CA0&amp;index=16">WATCH</a> <a href="https://www.youtube.com/watch?v=_aVPI0FeT70" class="fancybox-media img"><img src="/images/hp_images/rfi-more-arrow.gif" alt="" class="rfi-inline1" width="15" height="12" /></a></p>
+</div>
+<div class="rfi-clearit"> </div>
+</div>
